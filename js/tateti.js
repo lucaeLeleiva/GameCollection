251b37858c2601808650d2ed4bf05c4699ae906b
LucaLeiva <lucaleiva248@gmail.com>
2016-08-18 15:54:45 +0000

    Agrego metodo verificar tablero para reducir complejidad

--- js/tateti.js
+++ js/tateti.js
@@ -7,115 +7,74 @@ function actualizarMarcador(){
     var marcador=document.getElementById("marcador");
     marcador.innerHTML=ganadosJugador1+"-"+ganadosJugador2;
 }
+function verificarTablero(valor){
+    var botonAElegir;
+    var tablero = document.getElementById("tablerotateti");
+    var botones = tablero.getElementsByTagName("input");
+    if(botones[0].value===botones[1].value&&botones[0].value===valor&&botones[2].value===" "){
+        botonAElegir=botones[2];
+    }else if(botones[0].value===botones[2].value&&botones[0].value===valor&&botones[1].value===" "){
+        botonAElegir=botones[1];
+    }else if(botones[1].value===botones[2].value&&botones[1].value===valor&&botones[0].value===" "){
+        botonAElegir=botones[0];
+    }else if(botones[3].value===botones[4].value&&botones[3].value===valor&&botones[5].value===" "){
+        botonAElegir=botones[5];
+    }else if(botones[4].value===botones[5].value&&botones[4].value===valor&&botones[3].value===" "){
+        botonAElegir=botones[3];
+    }else if(botones[3].value===botones[5].value&&botones[3].value===valor&&botones[4].value===" "){
+        botonAElegir=botones[4];
+    }else if(botones[6].value===botones[7].value&&botones[6].value===valor&&botones[8].value===" "){
+        botonAElegir=botones[8];
+    }else if(botones[7].value===botones[8].value&&botones[7].value===valor&&botones[6].value===" "){
+        botonAElegir=botones[6];
+    }else if(botones[6].value===botones[8].value&&botones[6].value===valor&&botones[7].value===" "){
+        botonAElegir=botones[7];
+    }else if(botones[0].value===botones[3].value&&botones[0].value===valor&&botones[6].value===" "){
+        botonAElegir=botones[6];
+    }else if(botones[0].value===botones[6].value&&botones[0].value===valor&&botones[3].value===" "){
+        botonAElegir=botones[3];
+    }else if(botones[3].value===botones[6].value&&botones[3].value===valor&&botones[0].value===" "){
+        botonAElegir=botones[0];
+    }else if(botones[1].value===botones[4].value&&botones[1].value===valor&&botones[7].value===" "){
+        botonAElegir=botones[7];
+    }else if(botones[1].value===botones[7].value&&botones[1].value===valor&&botones[4].value===" "){
+        botonAElegir=botones[4];
+    }else if(botones[7].value===botones[4].value&&botones[7].value===valor&&botones[1].value===" "){
+        botonAElegir=botones[1];
+    }else if(botones[2].value===botones[5].value&&botones[2].value===valor&&botones[8].value===" "){
+        botonAElegir=botones[8];
+    }else if(botones[2].value===botones[8].value&&botones[2].value===valor&&botones[5].value===" "){
+        botonAElegir=botones[5];
+    }else if(botones[8].value===botones[5].value&&botones[8].value===valor&&botones[2].value===" "){
+        botonAElegir=botones[2];
+    }else if(botones[4].value===botones[8].value&&botones[4].value===valor&&botones[0].value===" "){
+        botonAElegir=botones[0];
+    }else if(botones[0].value===botones[4].value&&botones[0].value===valor&&botones[8].value===" "){
+        botonAElegir=botones[8];
+    }else if(botones[8].value===botones[0].value&&botones[8].value===valor&&botones[4].value===" "){
+        botonAElegir=botones[4];
+    }else if(botones[2].value===botones[4].value&&botones[2].value===valor&&botones[6].value===" "){
+        botonAElegir=botones[6];
+    }else if(botones[4].value===botones[6].value&&botones[4].value===valor&&botones[2].value===" "){
+        botonAElegir=botones[2];
+    }else if(botones[6].value===botones[2].value&&botones[6].value===valor&&botones[4].value===" "){
+        botonAElegir=botones[4];
+    }
+    return botonAElegir;
+}
 //Elije un boton, lo desabilita y le asigna O para la maquina.
 //Prioriza en orden, ganar, no dejar ganar, aleatorio.
 function ia(){
-    var tablero = document.getElementById("tablerotateti");
-    var botones = tablero.getElementsByTagName("input");
     numeroDeTurno++;
     var botonAElegir;
     //Primero trata de ganar.
-    if(botones[0].value===botones[1].value&&botones[0].value==="O"&&botones[2].value===" "){
-        botonAElegir=botones[2];
-    }else if(botones[0].value===botones[2].value&&botones[0].value==="O"&&botones[1].value===" "){
-        botonAElegir=botones[1];
-    }else if(botones[1].value===botones[2].value&&botones[1].value==="O"&&botones[0].value===" "){
-        botonAElegir=botones[0];
-    }else if(botones[3].value===botones[4].value&&botones[3].value==="O"&&botones[5].value===" "){
-        botonAElegir=botones[5];
-    }else if(botones[4].value===botones[5].value&&botones[4].value==="O"&&botones[3].value===" "){
-        botonAElegir=botones[3];
-    }else if(botones[3].value===botones[5].value&&botones[3].value==="O"&&botones[4].value===" "){
-        botonAElegir=botones[4];
-    }else if(botones[6].value===botones[7].value&&botones[6].value==="O"&&botones[8].value===" "){
-        botonAElegir=botones[8];
-    }else if(botones[7].value===botones[8].value&&botones[7].value==="O"&&botones[6].value===" "){
-        botonAElegir=botones[6];
-    }else if(botones[6].value===botones[8].value&&botones[6].value==="O"&&botones[7].value===" "){
-        botonAElegir=botones[7];
-    }else if(botones[0].value===botones[3].value&&botones[0].value==="O"&&botones[6].value===" "){
-        botonAElegir=botones[6];
-    }else if(botones[0].value===botones[6].value&&botones[0].value==="O"&&botones[3].value===" "){
-        botonAElegir=botones[3];
-    }else if(botones[3].value===botones[6].value&&botones[3].value==="O"&&botones[0].value===" "){
-        botonAElegir=botones[0];
-    }else if(botones[1].value===botones[4].value&&botones[1].value==="O"&&botones[7].value===" "){
-        botonAElegir=botones[7];
-    }else if(botones[1].value===botones[7].value&&botones[1].value==="O"&&botones[4].value===" "){
-        botonAElegir=botones[4];
-    }else if(botones[7].value===botones[4].value&&botones[7].value==="O"&&botones[1].value===" "){
-        botonAElegir=botones[1];
-    }else if(botones[2].value===botones[5].value&&botones[2].value==="O"&&botones[8].value===" "){
-        botonAElegir=botones[8];
-    }else if(botones[2].value===botones[8].value&&botones[2].value==="O"&&botones[5].value===" "){
-        botonAElegir=botones[5];
-    }else if(botones[8].value===botones[5].value&&botones[8].value==="O"&&botones[2].value===" "){
-        botonAElegir=botones[2];
-    }else if(botones[4].value===botones[8].value&&botones[4].value==="O"&&botones[0].value===" "){
-        botonAElegir=botones[0];
-    }else if(botones[0].value===botones[4].value&&botones[0].value==="O"&&botones[8].value===" "){
-        botonAElegir=botones[8];
-    }else if(botones[8].value===botones[0].value&&botones[8].value==="O"&&botones[4].value===" "){
-        botonAElegir=botones[4];
-    }else if(botones[2].value===botones[4].value&&botones[2].value==="O"&&botones[6].value===" "){
-        botonAElegir=botones[6];
-    }else if(botones[4].value===botones[6].value&&botones[4].value==="O"&&botones[2].value===" "){
-        botonAElegir=botones[2];
-    }else if(botones[6].value===botones[2].value&&botones[6].value==="O"&&botones[4].value===" "){
-        botonAElegir=botones[4];
-    }
+    botonAElegir=verificarTablero("O");
     //Si no encontro un boton que le consiga la victoria busca evitar que el jugador gane.
     if(botonAElegir==null){
-        if(botones[0].value===botones[1].value&&botones[0].value==="X"&&botones[2].value===" "){
-            botonAElegir=botones[2];
-        }else if(botones[0].value===botones[2].value&&botones[0].value==="X"&&botones[1].value===" "){
-            botonAElegir=botones[1];
-        }else if(botones[1].value===botones[2].value&&botones[1].value==="X"&&botones[0].value===" "){
-            botonAElegir=botones[0];
-        }else if(botones[3].value===botones[4].value&&botones[3].value==="X"&&botones[5].value===" "){
-            botonAElegir=botones[5];
-        }else if(botones[4].value===botones[5].value&&botones[4].value==="X"&&botones[3].value===" "){
-            botonAElegir=botones[3];
-        }else if(botones[3].value===botones[5].value&&botones[3].value==="X"&&botones[4].value===" "){
-            botonAElegir=botones[4];
-        }else if(botones[6].value===botones[7].value&&botones[6].value==="X"&&botones[8].value===" "){
-            botonAElegir=botones[8];
-        }else if(botones[7].value===botones[8].value&&botones[7].value==="X"&&botones[6].value===" "){
-            botonAElegir=botones[6];
-        }else if(botones[6].value===botones[8].value&&botones[6].value==="X"&&botones[7].value===" "){
-            botonAElegir=botones[7];
-        }else if(botones[0].value===botones[3].value&&botones[0].value==="X"&&botones[6].value===" "){
-            botonAElegir=botones[6];
-        }else if(botones[0].value===botones[6].value&&botones[0].value==="X"&&botones[3].value===" "){
-            botonAElegir=botones[3];
-        }else if(botones[3].value===botones[6].value&&botones[3].value==="X"&&botones[0].value===" "){
-            botonAElegir=botones[0];
-        }else if(botones[1].value===botones[4].value&&botones[1].value==="X"&&botones[7].value===" "){
-            botonAElegir=botones[7];
-        }else if(botones[1].value===botones[7].value&&botones[1].value==="X"&&botones[4].value===" "){
-            botonAElegir=botones[4];
-        }else if(botones[7].value===botones[4].value&&botones[7].value==="X"&&botones[1].value===" "){
-            botonAElegir=botones[1];
-        }else if(botones[2].value===botones[5].value&&botones[2].value==="X"&&botones[8].value===" "){
-            botonAElegir=botones[8];
-        }else if(botones[2].value===botones[8].value&&botones[2].value==="X"&&botones[5].value===" "){
-            botonAElegir=botones[5];
-        }else if(botones[8].value===botones[5].value&&botones[8].value==="X"&&botones[2].value===" "){
-            botonAElegir=botones[2];
-        }else if(botones[4].value===botones[8].value&&botones[4].value==="X"&&botones[0].value===" "){
-            botonAElegir=botones[0];
-        }else if(botones[0].value===botones[4].value&&botones[0].value==="X"&&botones[8].value===" "){
-            botonAElegir=botones[8];
-        }else if(botones[8].value===botones[0].value&&botones[8].value==="X"&&botones[4].value===" "){
-            botonAElegir=botones[4];
-        }else if(botones[2].value===botones[4].value&&botones[2].value==="X"&&botones[6].value===" "){
-            botonAElegir=botones[6];
-        }else if(botones[4].value===botones[6].value&&botones[4].value==="X"&&botones[2].value===" "){
-            botonAElegir=botones[2];
-        }else if(botones[6].value===botones[2].value&&botones[6].value==="X"&&botones[4].value===" "){
-            botonAElegir=botones[4];
-        } 
+        botonAElegir=verificarTablero("X");
     }
+    var tablero = document.getElementById("tablerotateti");
+    var botones = tablero.getElementsByTagName("input");
     //Si no se cumple ninguno de los otros casos elige un boton random.
     while(botonAElegir==null||botonAElegir.value==="X"||botonAElegir.value==="O"){
         var numeroDeBoton=Math.floor(Math.random()*(9 - 0));
